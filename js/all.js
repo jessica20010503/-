e81308c2c5e90ac004d4a342a31a05245e88dd3a
奕慈 <jessica20010503@gmail.com>
2021-12-17 12:18:20 +0800

    兩邊合併之錯誤修正

--- js/all.js
+++ js/all.js
@@ -38,3 +38,17 @@ $(document).ready(function () {
 	}
 	setTotal();
 });
+$(document).ready(function(){
+    $('.font-s').click(function (e) { 
+      $('.news-content .article p').css('font-size','14px');
+    });
+  
+    $('.font-m').click(function (e) { 
+      $('.news-content .article p').css('font-size','18px');
+    });
+  
+    $('.font-b').click(function (e) { 
+      $('.news-content .article p').css('font-size','20px');
+    });
+  
+  });
